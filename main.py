a5cfa12a762975f450d7ff48629299cdea5e21d5
APelia99097 <anshveerp097@deltalearns.ca>
2025-03-23 22:25:47 +0000

    finsihed the encrypted message, this took awhile becuase of the shift value and stuff i actually found someone talking about this on reddit which is how i managed to figure it out

--- main.py
+++ main.py
@@ -2,7 +2,15 @@
 # Cypher: Caeser Cipher
 
 # Encrypt Message
-
+def encrypt_message(plaintext, shift):
+    encrypted_message = ""
+    for charecter in plaintext:
+        if charecter.isalpha():
+            shift_value = 65 if charecter.isupper() else 97 # this took awhile to figure out 65 = A while 97 = a so upper/lower
+            encrypted_text += chr((ord(charecter) - shift_value + shift) % 26 + shift_value)
+        else:
+            encrypted_text += charecter # numerical values stay the same
+    return encrypted_message
 
 # Decrypt Message
 
