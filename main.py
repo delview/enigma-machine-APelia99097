065f8073ee1485a04fe1d4b70b027072d4ffbbff
APelia99097 <anshveerp097@deltalearns.ca>
2025-03-23 22:41:46 +0000

    Finsihed decrypt message, it was almost idetincal to encrypt extcept for minusing the shift value to return the message to its original state

--- main.py
+++ main.py
@@ -3,15 +3,23 @@
 
 # Encrypt Message
 def encrypt_message(plaintext, shift):
-    encrypted_message = ""
+    encrypted_text = ""
     for charecter in plaintext:
         if charecter.isalpha():
             shift_value = 65 if charecter.isupper() else 97 # this took awhile to figure out 65 = A while 97 = a so upper/lower
             encrypted_text += chr((ord(charecter) - shift_value + shift) % 26 + shift_value)
         else:
             encrypted_text += charecter # numerical values stay the same
-    return encrypted_message
+    return encrypted_text
 
 # Decrypt Message
-
+def decrypt_message(encrypted_text, shift):
+    decrypted_text = ""
+    for charecter in encrypted_text:
+        if charecter.isaplpha():
+            shift_value = 65 if charecter.isupper() else 97  # ASCII value for A/a
+            decrypted_text += chr((ord(charecter) - shift_value - shift) % 26 + shift_value)
+        else:
+            decrypted_text += charecter  # Non-alphabet characters remain unchanged
+    return decrypted_text
 # Main Body
